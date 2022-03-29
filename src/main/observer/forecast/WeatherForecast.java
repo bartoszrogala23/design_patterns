e8871d7d8cbafd7cfac90fa5a4fd4accb7360a73
brogala <brogala@sii.pl>
2022-03-29 10:13:38 +0200

    method implementation

--- src/main/observer/forecast/WeatherForecast.java
+++ src/main/observer/forecast/WeatherForecast.java
@@ -3,6 +3,7 @@ package observer.forecast;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import observer.notification.Observer;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -22,16 +23,18 @@ public class WeatherForecast implements Observable {
 
     @Override
     public void registerObserver(Observer observer) {
-
+registeredObservers.add(observer);
     }
 
     @Override
     public void unregisterObserver(Observer observer) {
-
+registeredObservers.remove(observer);
     }
 
     @Override
     public void notifyObservers() {
-
+        for (Observer observer : registeredObservers) {
+            observer.updateForecast(this);
+        }
     }
 }
